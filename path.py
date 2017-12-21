975cba2872bdbd0563f51d1818087085f3c21759
hermes <2587196108@qq.com>
2017-12-21 17:50:02 +0800

    third

---
+++ path.py
@@ -0,0 +1,6 @@
+import os
+print(__file__)
+print(os.path.abspath(__file__))
+print(os.path.dirname(os.path.abspath(__file__)))
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print('/'.join(__file__.split("/")[:-2]))
